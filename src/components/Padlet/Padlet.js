90d5ad0034cfd8552ca29b6da5748b582a670e80
Ayushi Amin <amina23@mcmaster.ca>
2024-08-27 14:37:50 -0400

    hid padlet src

--- src/components/Padlet/Padlet.js
+++ src/components/Padlet/Padlet.js
@@ -5,7 +5,7 @@ const PadletEmbed = () => {
     <div class="padlet-embed" style="border:1px solid rgba(0,0,0,0.1);border-radius:2px;box-sizing:border-box;overflow:hidden;position:relative;width:100%;background:#F4F4F4">
       <p style="padding:0;margin:0">
         <iframe
-          src="https://padlet.com/embed/pcxkh53ez31p50z5"
+          src="${process.env.REACT_APP_PADLET_URL}"
           frameborder="0"
           allow="fullscreen;camera;microphone;geolocation"
           style="width:100%;height:608px;display:block;padding:0;margin:0"
